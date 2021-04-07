399eb7ee55b5b37bc80bdd5f6bf493d82d96c029
jonyelson <jonyelson20@gmail.com>
2021-04-07 18:25:51 -0400

    Feat: created job functionality

--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,25 @@ const views = __dirname + "/views/"
 
 const profile = {
   name: 'Jonyelson Lopes',
-  avatar: 'https://avatars.githubusercontent.com/u/55725692?v=4',
+  avatar: 'https://github.com/JL-Sousa',
   "monthly-budget": 2000,
   "hours-per-day": 5,
   "days-per-week": 5,
   "vacation-per-year": 4
 }
 
+const jobs = []
+
 routes.get('/', (request, response) => response.render(views + "index"))
 routes.get('/job', (request, response) => response.render(views + 'job'))
+routes.post('/job', (request, response) => {
+
+  const job = request.body
+  job.createdAt = Date.now()
+
+  jobs.push()
+  return response.redirect('/')
+})
 routes.get('/job/edit', (request, response) => response.render(views + 'job-edit'))
 routes.get('/profile', (request, response) => response.render(views + 'profile', { profile }))
 
